e60b67cee70b1883df53e836030dfa9140754a05
giselleguillon <119024271+giselleguillon@users.noreply.github.com>
2023-08-03 22:23:23 -0300

    cardsjs

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
-const Productdata = [
+const productos = [
     {
         id: 1,
         name: "Zapatillas Roma",
-        Bid: 200,
+        bid: 200,
         category: "niños",
         cardImg: "./imagenes/zapatillasroma.jpg",
 
@@ -32,6 +32,80 @@ const Productdata = [
         category: "niños",
         cardImg:"./imagenes/zapatillasafrica.png",
         
+    },
+
+    {
+        id: 5,
+        name: "Zapatillas Brasil",
+        bid: 300,
+        category: "mujer",
+        cardImg: "./imagenes/zapatillabrasil.jpg",
+    },
+
+    {
+        id: 6,
+        name: "Zapatillas china",
+        bid: 200,
+        category: "mujer",
+        cardImg: "./imagenes/zapatillachina.png",
+    },
+
+    {
+        id: 7,
+        name: "Zapatillas Suiza",
+        bid: 250,
+        category: "mujer",
+        cardImg: "./imagenes/zapatillasuiza.jpg",
+    },
+    {
+        id: 8,
+        name: "Zapatillas Panama ",
+        bid: 200,
+        category: "hombre",
+        cardImg: "./imagenes/zapatillapanama.jpg",
+    },
+
+    {
+        id: 9,
+        name: "Zapatillas Peru",
+        bid: 200,
+        category: "mujer",
+        cardImg: "./imagenes/zapatillasperu.png",
+    },
+    {
+        id: 10,
+        name: "Zapatillas Canada",
+        bid: 280,
+        category: "hombre",
+        cardImg: "./imagenes/zapatillascanada.png",
+    },
+
+    {
+        id: 11,
+        name: "Zapatillas Suecia ",
+        bid: 200,
+        category: "hombre",
+        cardImg: "./imagenes/zapatillasuecia.jpg",
     }
 
 ];
+
+const divideProductsInParts = (size) => {
+    let productList = [];
+    for (let index = 0; index < productos.length; index += size) {
+        productList.push(productos.slice( index, index + size));
+
+    }
+    return productList;
+};
+
+console.log (divideProductsInParts(2));
+
+
+const appState = {
+    products: divideProductsInParts(2),
+    currentProductsIndex: 0,
+    productsinit: divideProductsInParts(2).length,
+    activeFilter: null,
+};
+
